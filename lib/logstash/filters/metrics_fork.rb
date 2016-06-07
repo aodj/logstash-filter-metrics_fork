306808eba4e65b250d3a925e4c098f157097a3fb
Alexander O'Donovan-Jones <alexander@dohop.com>
2016-06-07 15:08:06 +0000

    Adding tests, and making some code changes

--- lib/logstash/filters/metrics_fork.rb
+++ lib/logstash/filters/metrics_fork.rb
@@ -7,13 +7,18 @@ require "logstash/namespace"
 class LogStash::Filters::MetricsFork < LogStash::Filters::Base
   config_name "metrics_fork"
 
-  # The field containing the metric to use as the value. This is usually either count or
-  # one of the rate metrics (f.ex: rate_1m)
-  config :metric, :validate => :string, :default => "count", :required => true
-  
-  # The prefix to match in the event for the metric fields 
+  # The prefix to match in the event for the metric fields. Everything after the prefix
+  # in the field name is considered the name of the extracted metric 
   config :prefix, :validate => :string
 
+  # The field containing the metric to use as the value. This is usually either count or
+  # one of the rate metrics (f.ex: rate_1m)
+  config :field, :validate => :string, :default => "count", :required => true
+  
+  # The name to use for the metric. if this isn't set, the filter will use the @prefix 
+  # value instead
+  config :name, :validate => :string
+  
   public
   def register
     # Add instance variables
@@ -25,33 +30,36 @@ class LogStash::Filters::MetricsFork < LogStash::Filters::Base
     return unless filter?(event)
 
     # extract the metrics and the value we're interested in. if @prefix=>"status_code" and
-    # @metric=>"count" this:
+    # @field=>"count" this:
     # {"status_code.200"=>{"count"=>24, ...}, "status_code.400"=>{"count"=>17, ...}}
     # becomes:
     # {"status_code.200"=>24, "status_code.400"=>17}
     relevant_fields = {}
-    event.each { |key,value|
+    event.to_hash.each do |key,value|
       if key.start_with?(@prefix) 
-        metric_value = event[key][@metric]
+        metric_value = event.get("[%s][%s]" % [key, @field])
         relevant_fields[key] = metric_value
       end
-    }
+    end
     
     # remove the original metrics from the event
-    relevant_fields.keys.each { |key|
+    relevant_fields.keys.each do |key|
       event.remove(key)
-    }
+    end
     
     # clone the event, reformat the metric and emit the new event
-    relevant_fields.each { |key,value|
+    relevant_fields.each do |key,value|
       fork = event.clone
-      # this gives us a new event with {'status_code'=>24, 'metric'=>'200'} 
-      fork.set(@prefix, value)
-      metric_name = key[@prefix.length..-1]
       
-      fork.set('metric', metric_name)
+      # this gives us a new event with {'status_code'=>24, 'metric'=>'200'} 
+      if @name.nil?
+        fork.set(@prefix, value) # {'status_code'=>24}
+      else
+        fork.set(@name, value) # {'status_code'=>24}
+      end
+      fork.set('metric', key[@prefix.length..-1]) # {'metric'=>200}
       yield fork
-    }
+    end
 
     # cancel the triggering event
     event.cancel
--- spec/filters/metrics_fork_spec.rb
+++ spec/filters/metrics_fork_spec.rb
@@ -1,20 +1,60 @@
 # encoding: utf-8
-require 'spec_helper'
-require "logstash/filters/example"
+require "spec_helper"
+require "logstash/filters/metrics_fork"
 
-describe LogStash::Filters::Example do
-  describe "Set to Hello World" do
-    let(:config) do <<-CONFIG
+describe LogStash::Filters::MetricsFork do
+  describe "test with prefix and field" do
+    config <<-CONFIG
       filter {
-        example {
-          message => "Hello World"
+        metrics_fork {
+          prefix => "status_code."
+          field => "count"
         }
       }
     CONFIG
+  
+    sample("status_code.200"=>{"count"=>24}, "status_code.400"=>{"count"=>17}) do
+      insist { subject.length } == 2
+      insist { subject[0].get("status_code.") } == 24
+      insist { subject[0].get("metric") } == "200"
+      insist { subject[1].get("status_code.") } == 17
+      insist { subject[1].get("metric") } == "400"
     end
-
-    sample("message" => "some text") do
-      expect(subject.get("message")).to eq('Hello World')
+  end
+  
+  describe "test with prefix, field and override name" do
+    config <<-CONFIG
+      filter {
+        metrics_fork {
+          prefix => "status_code."
+          field => "count"
+          name => "http_status_code"
+        }
+      }
+    CONFIG
+  
+    sample("status_code.200"=>{"count"=>24}, "status_code.400"=>{"count"=>17}) do
+      insist { subject.length } == 2
+      insist { subject[0].get("http_status_code") } == 24
+      insist { subject[0].get("metric") } == "200"
+      insist { subject[1].get("http_status_code") } == 17
+      insist { subject[1].get("metric") } == "400"
+    end
+  end
+  
+  describe "test with short prefix" do
+    config <<-CONFIG
+      filter {
+        metrics_fork {
+          prefix => "stat"
+          field => "rate_1m"
+        }
+      }
+    CONFIG
+  
+    sample("status_code.200"=>{"count"=>24, "rate_1m"=>15}) do
+      insist { subject.get("stat") } == 15
+      insist { subject.get("metric") } == "us_code.200"
     end
   end
 end
